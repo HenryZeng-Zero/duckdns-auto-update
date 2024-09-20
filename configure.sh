188e64b57b99afe0c504a7890e431999060e2b9c
HenryZeng <zengdeveloper@qq.com>
2024-09-20 17:00:04 +0800

    replace yaml process function to "yq"

--- configure.sh
+++ configure.sh
@@ -2,21 +2,18 @@
 
 TRUE=1
 FALSE=0
-
-LINE_IS_VALID_ANNOTATION=0
-LINE_IS_INVALID_ANNOTATION=1
-LINE_IS_TARGET=2
-LINE_NOT_TARGET=3
+RETURN_DATA=''
 
 function help() {
     echo "configure.sh [options]"
     echo "   -h, --help                           Show this help message"
     echo "   -i, --init                           Init config file"
+    echo "   -d, --depend                         Install dependencies with apt"
     echo "   -l, --list                           List all tags in config file"
+    echo "   -m, --more [tag]                     List more info about specific tag"
     echo
-    echo "   -a, --add                            Add config file"
-    echo "-> -a [tag] [domains] [token]"
-    echo "-> eg) -i duck-test duck-test.duckdns.org 5a6b4e33-2e82-431b-9684-1c76d03f3af1"
+    echo "   -a, --add [tag] [domains] [token]    Add config file"
+    echo "-> eg) -a duck-test duck-test.duckdns.org 5a6b4e33-2e82-431b-9684-1c76d03f3af1"
     echo
     echo "   -c, --clean                          Remove config file"
 }
@@ -37,84 +34,18 @@ cat > config << EOF
 EOF
 }
 
-function annotation_syntax_tip_error_header(){
-    # $1 : line data
-    # $2 : line count
-
-    echo -e "\e[31m[syntaxError]\e[0m at line \e[100m$2\e[0m"
-    echo -e "\e[93m<example>\e[0m annotation:"
-    echo "# annotation"
-    echo "    # annotation"
-    echo -e "\e[93m<mistake line>\e[0m"
-    echo -e "\e[44m$1\e[0m"
+function config_get_tag(){
+    tag=$(echo $1 | grep -o -P '.*?:.*?#*.*')
+    echo $tag
 }
 
-function is_target() {
-    sub=$1
-
-    if [[ $sub =~ ":" ]]; then
-        sub=${line%:*} # 提取 ':' 左侧内容
-
-        # 
-        if [[ "$sub" =~ "#" ]]; then
-            return $LINE_NOT_TARGET
-        fi
-
-        # 如果左侧非空表示Target结构
-        if [ -n "$sub" ]; then
-            return $LINE_IS_TARGET
-        fi
-    fi
-
-    return $LINE_NOT_TARGET
-}
-
-function annotation_syntax_check(){
-    # $1 : line data
-    # $2 : line count
-
-    sub=$1
-
-    is_target $sub
-
-    if [ $? -eq $LINE_IS_TARGET ]; then
-        return $LINE_IS_TARGET
-    fi
-
-    if [[ "$sub" =~ "#" ]]; then
-        sub=${line%#*} # 提取 '#' 左侧内容
-        sub=$(echo $sub | grep -o -P '[a-zA-Z:]*')
-
-        # 如果 '#' 左侧符合 target: xxx 模式，则依然表示vz
-        if [[ $sub =~ ":" ]]; then
-            return $LINE_IS_TARGET
-        fi
-        
-        if [ -z "$sub" ]; then
-            return $LINE_IS_VALID_ANNOTATION
-        else
-            annotation_syntax_tip_error_header $1 $2
-            echo -e "\e[93m<repair>\e[0m"
-            echo -e "\e[96m1. Shoud not add letters in front of symbol '#'.\e[0m"
-            echo -e "\e[96m2. Or fogot add ':' before Target name.\e[0m"
-            exit 1
-        fi
-    fi
-
-    
-
-    # it also represents the line is not a annotation
-    return $LINE_IS_INVALID_ANNOTATION
-}
 function config_tags_list(){
     IFS=''
     separate_count=1
 
     while IFS= read -r line
     do
-        annotation_syntax_check $line $separate_count
-        echo "OK: $?"
-        # if [ $? -eq $FALSE ]; then
+        config_get_tag $line
 
         separate_count=$(( $separate_count + 1 ))
     done < config
@@ -136,6 +67,9 @@ case $1 in
     ;;
     -l|--list)
         config_tags_list
+    ;;
+    -d|--depend)
+        apt install -y yq
     ;;
     -c|--clean)
         rm config
